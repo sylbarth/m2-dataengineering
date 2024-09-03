14fa6814f40a87863e5b64981d26632e758cb370
Sylvain Barthélémy <sylbarth@gmail.com>
2024-09-03 09:19:56 +0200

    exemples

--- docker/mariadb/README.md
+++ docker/mariadb/README.md
@@ -15,4 +15,4 @@ docker ps
 On se connecte à la base de données sur localhost (port 3306)
 
 # Interface graphique (phpMyAdmin)
-On se connecte sur [http://localhost:8080](http://localhost:8080) pour administer les bases de données Mariadb
+On se connecte sur [http://localhost:8080](http://localhost:8080) pour administer les bases de données Mariadb (user: root, pass: master2)
---
+++ python/data/query.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.sql import text
+
+DB = "mysql://root:master2@localhost/projet"
+
+engine = sqlalchemy.create_engine(DB)
+
+# exécuter du SQL directement
+with engine.connect() as con:
+    con.execute(text(f"DELETE FROM articles WHERE id=1"))
+    con.commit()
+
+# exécuter via pandas
+data = pd.read_csv("auteurs.csv")
+data.to_sql(name="auteurs", con=engine, if_exists='append', index=False)
